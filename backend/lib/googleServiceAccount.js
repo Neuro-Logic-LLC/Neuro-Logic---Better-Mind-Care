fd3227e6dd06a7cfc896b0669d359d32b340bdff
JerodBMC <jerod@bettermindcare.com>
2025-11-10 14:50:48 -0600

    calendar fix1110-1

--- backend/lib/googleServiceAccount.js
+++ backend/lib/googleServiceAccount.js
@@ -6,7 +6,7 @@ function getServiceAccountClient(impersonateEmail) {
     email: 'bmc-calendar-access@tactical-prism-468521-f6.iam.gserviceaccount.com',
     key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     scopes: ['https://www.googleapis.com/auth/calendar.events'],
-    subject: impersonateEmail, // e.g. jim@bettermindcare.com
+    subject: 'jim@bettermindcare.com', // e.g. jim@bettermindcare.com
   });
   return google.calendar({ version: 'v3', auth });
 }
