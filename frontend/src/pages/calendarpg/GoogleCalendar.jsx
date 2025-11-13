0916ac978b30dc57acada8296a27c95c6beb963a
JerodBMC <jerod@bettermindcare.com>
2025-11-13 15:22:32 -0600

    fix: onSelectSlot in GoogleCalendar.jsx

--- frontend/src/pages/calendarpg/GoogleCalendar.jsx
+++ frontend/src/pages/calendarpg/GoogleCalendar.jsx
@@ -267,6 +267,7 @@ export default function GoogleCalendar() {
         calendarId,
         includePastDays
       );
+      console.log("RAW EVENTS:", evs);
       setEvents(
         (evs || []).map((e) => ({
           ...e,
