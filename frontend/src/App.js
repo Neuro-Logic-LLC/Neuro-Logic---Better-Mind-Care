af786c0b06ce71a237a4bed6b6741c9240a3451f
Ashley Broussard <fleurdeviefarmsllc@gmail.com>
2025-11-25 13:39:49 -0600

    Remove all ProtectedRoute wrappers from routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,6 @@ import ScreeningOrder from './pages/screeningorderpg/ScreeningOrder';
 import UserList from './pages/userslistpg/UserList';
 import AuditLog from '../src/pages/auditpg/AuditLog';
 import { AuthProvider } from './auth/AuthContext';
-import ProtectedRoute from './auth/ProtectedRoute';
 import Login from './pages/loginpg/login';
 import SignUp from './pages/signuppg/SignUp';
 import ForgotPassword from './pages/forgotpasswordpg/ForgotPassword';
@@ -65,22 +64,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-             <Route
-               path="/evexia-lab-results"
-               element={
-                 <ProtectedRoute>
-                   <EvexiaLabReport />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/google-calendar"
-               element={
-                 <ProtectedRoute>
-                   <GoogleCalendar />
-                 </ProtectedRoute>
-               }
-             />
+             <Route path="/evexia-lab-results" element={<EvexiaLabReport />} />
+             <Route path="/google-calendar" element={<GoogleCalendar />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/success" element={<SuccessPage />} />
             <Route path="/cancel-order" element={<CancelPage />} />
@@ -88,160 +73,34 @@ function App() {
             <Route path="/terms" element={<TermsOfService />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
 
-            <Route
-              path="/admin/users/:id"
-              element={
-                <ProtectedRoute>
-                  <UserDetails />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/intake-form"
-              element={
-                <ProtectedRoute>
-                  <IntakeForm />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/report"
-              element={
-                <ProtectedRoute>
-                  <PatientReport />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/my-reports"
-              element={
-                <ProtectedRoute>
-                  <MyReports />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin/users"
-              element={
-                <ProtectedRoute>
-                  <UserList />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin/logs"
-              element={
-                <ProtectedRoute>
-                  <AuditLog />
-                </ProtectedRoute>
-              }
-            />
+             <Route path="/admin/users/:id" element={<UserDetails />} />
+             <Route path="/intake-form" element={<IntakeForm />} />
+             <Route path="/report" element={<PatientReport />} />
+             <Route path="/my-reports" element={<MyReports />} />
+             <Route path="/admin/dashboard" element={<Dashboard />} />
+             <Route path="/admin/users" element={<UserList />} />
+             <Route path="/admin/logs" element={<AuditLog />} />
 
-            <Route
-              path="/screening-order"
-              element={
-                <ProtectedRoute>
-                  <ScreeningOrder />
-                </ProtectedRoute>
-              }
-            />
+             <Route path="/screening-order" element={<ScreeningOrder />} />
 
-            <Route
-              path="/patient-orders"
-              element={
-                <ProtectedRoute>
-                  <PatientOrders />
-                </ProtectedRoute>
-              }
-            />
+             <Route path="/patient-orders" element={<PatientOrders />} />
 
-             <Route
-               path="/automated-lab-results"
-               element={
-                 <ProtectedRoute>
-                   <AutomatedLabResults />
-                 </ProtectedRoute>
-               }
-             />
+             <Route path="/automated-lab-results" element={<AutomatedLabResults />} />
 
-             <Route
-               path="/patient-req"
-               element={
-                 <ProtectedRoute>
-                   <PatientRequisitionViewer />
-                 </ProtectedRoute>
-               }
-             />
+             <Route path="/patient-req" element={<PatientRequisitionViewer />} />
 
             <Route path="/confirm-email" element={<ConfirmEmail />} />
 
              <Route path="/join" element={<EmailStep />} />
             <Route path="/join/checkout" element={<CheckoutStep />} />
-             <Route
-               path="/account-info"
-               element={
-                 <ProtectedRoute>
-                   <StepThreeAccountSetup />
-                 </ProtectedRoute>
-               }
-             />
+             <Route path="/account-info" element={<StepThreeAccountSetup />} />
              <Route path="/sign-up" element={<SignUp />} />
-             <Route
-               path="/account"
-               element={
-                 <ProtectedRoute>
-                   <Account />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/support"
-               element={
-                 <ProtectedRoute>
-                   <Support />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/messages"
-               element={
-                 <ProtectedRoute>
-                   <Messages />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/appointments"
-               element={
-                 <ProtectedRoute>
-                   <Appointments />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/faq"
-               element={
-                 <ProtectedRoute>
-                   <Resources />
-                 </ProtectedRoute>
-               }
-             />
-             <Route
-               path="/resources/articles/:slug"
-               element={
-                 <ProtectedRoute>
-                   <Article />
-                 </ProtectedRoute>
-               }
-             />
+             <Route path="/account" element={<Account />} />
+             <Route path="/support" element={<Support />} />
+             <Route path="/messages" element={<Messages />} />
+             <Route path="/appointments" element={<Appointments />} />
+             <Route path="/faq" element={<Resources />} />
+             <Route path="/resources/articles/:slug" element={<Article />} />
              <Route path="*" element={<NotFound />} />
           </Routes>
         </SignupProvider>
